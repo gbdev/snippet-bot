c73685f39990fd5a1216e0a2f8e7b6e2e26b91ad
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2017-11-26 10:09:33 -0600

    ignore config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const config = {};
 config.domains = ["pastebin.com"];	// pastebin/etc domans
 config.dbFilename = "database.db";	// the database file to store the collection
 config.history = false;			// archive chat history on login
-config.token = "PUT YOUR TOKEN HERE";	// discord login token
+config.token = "MzY2Mjk4NDEyNDQzODkzNzYx.DPnh3g.TBGJZPhB3NEdbGkKbfxir7cICHY";	// discord login token
 
 module.exports = config;
