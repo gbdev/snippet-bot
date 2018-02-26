73418a69440b10672ed8f2d761bf5fc9bd56ff1e
Aardbei <megaloler9000@gmail.com>
2018-02-26 12:57:49 -0600

    basic frontend

--- server.js
+++ server.js
@@ -15,59 +15,51 @@ var database;
 // exit on failure
 function prepareDatabase()
 {
-	// load the database from file
-	database = new sqlite3.Database(config.dbFilename, sqlite3.OPEN_READONLY, e => {
-		if(e) exit(`Error opening database: ${e}`);
-	});
+    // load the database from file
+    database = new sqlite3.Database(config.dbFilename, sqlite3.OPEN_READONLY, e => {
+	if(e) exit(`Error opening database: ${e}`);
+    });
 };
 
 // close the database if its open
 function closeDatabase()
 {
-	if(database) database.close().catch(console.error);
-}
-
-// get a row in a table
-function selectRow(table, whereColumn, equals, callback)
-{
-	// is this safe?
-	const sql = `SELECT * FROM ${table} WHERE ${whereColumn} = "${equals}"`;
-	database.all(sql, [], (e, rows) => {
-		if(e) exit(`Error querying database: ${e}`);
-		else callback(rows);
-	});
+    if(database) database.close().catch(console.error);
 }
 
 // take a timestamp and format it
 function formatDate(timestamp)
 {
-	return new Date(timestamp).toUTCString();
+    return new Date(timestamp).toUTCString();
 }
 
 // format a massege for code blocks
 function formatMessage(string)
 {
-        const regex = /\`\`\`(([a-z]+)\n)?\n*([\s\S]*?)\n*\`\`\`/g;
-	return string.replace(regex, (match, p1, p2, p3, offset, string) =>
-		`<div class="code">${p2 ? `<span class="lang">#${p2}</span><br />` : ""}${p3}</div>`);
+    const regex = /\`\`\`(([a-z]+)\n)?\n*([\s\S]*?)\n*\`\`\`/g;
+    const s = string.replace(regex, (match, p1, p2, p3, offset, string) =>
+			     `<div class="code">${p2 ? `<span class="lang">#${p2}</span><br />` : ""}${p3.replace(/\n/g, "<br />")}</div>`);
+    // also parse inline `code` and urls
+    return s;
 }
 
 // make html output for a message
-function makeMessagePage(message, revisions)
+function makeMessagePage(rows)
 {
-	// just something for now, i don't know what we actually want to do
-	const header = `
-<span class="field">Message ID:</span> <span class="value">${message.msgId}</span><br />
-<span class="field">Author:</span> <span class="value">${message.author}</span><br />
-<span class="field">Channel:</span> <span class="value">${message.channel}</span><br />
-	`;
-	const body = revisions.map(revision => `
+    const body = rows.map(row => `
+<a class="perma" href="/code?msgId=${row.msgId}">permalink</a><br />
+Author: ${row.author}<br />
+Channel: ${row.channel}<br />` + row.revisions.map(revision => `
 <span class="timestamp">${formatDate(revision.date)}</span>
 <div class="messageBody">${formatMessage(revision.fullMessage)}</div>
-	`).join("<br /><br />");
+	`).join("<br /><br />")).join("<br /><br />");
 
-	return `
+    return `
 <style>
+.perma
+{
+	font-size: small;
+}
 .wrapper
 {
 	width: 400px;
@@ -108,60 +100,122 @@ function makeMessagePage(message, revisions)
 </style>
 
 <div class="wrapper">
-	${header}
-	<br />
 	${body}
 </div>
 	`;
 }
 
-// request a message by id
-frontend.get("/code/:id", (request, response) => {
-	// select the message by id
-	selectRow("Message", "msgId", request.params.id, rows => {
-		if(rows.length)
+// search page
+frontend.get("/", (request, response) => {
+    const sql = `SELECT DISTINCT author FROM Message`;
+    database.all(sql, [], (e, authors) => {
+	if(e) exit(`Error querying database: ${e}`);
+	else
+	{
+	    const sql = `SELECT DISTINCT channel FROM Message`;
+	    database.all(sql, [], (e, channels) => {
+		if(e) exit(`Error querying database: ${e}`);
+		else
 		{
-			// assuming there's only one message per id
-			// as there should be
-			const message = rows[0];
-
-			// select all the revisions of this message
-			selectRow("Content", "associatedMsg", message.msgId, rows => {
-				if(rows.length)
-				{
-					// should we sort in sqlite instead of here?
-					// sort the revisions by timestamp
-					const sorted = rows.sort((a, b) => a.date - b.date);
-
-					// what should we actually be displaying here?
-					// a pretty display for the user?
-					// or some raw information?
-					// also, should we just show the most recent revision?
-					response.send(makeMessagePage(message, rows));
-				}
-				else response.send("No revisions found!");
-			});
+		    response.send(`
+<form action="/code/">
+<label for="author">Author</label>
+<select id="author" name="author">
+<option value="">*</option>
+${authors.map(author => `<option value="${author.author}">${author.author}</option>`).join("")}
+</select><br />
+<label for="channel">Channel</label>
+<select id="channel" name="channel">
+<option value="">*</option>
+${channels.map(channel => `<option value="${channel.channel}">${channel.channel}</option>`).join("")}
+</select><br />
+<label for="from">From</label>
+<input id="from" name="from" type="date"><br />
+<label for="to">To</label>
+<input id="to" name="to" type="date"><br />
+<input type="submit">
+</form>
+`);
 		}
-		else response.send("Message not found!");
-	});
+	    });
+	}
+    });
+});
+
+function strtotime(string)
+{
+    return new Date(string).getTime();
+}
+
+// filter messages query
+frontend.get("/code/", (request, response) => {
+    var sql = `SELECT * FROM Message`;
+    const bindings = [];
+
+    var kw = "WHERE";
+    if(request.query.author)
+    {
+	sql += ` ${kw} author = ?`;
+	bindings.push(request.query.author);
+	kw = "AND";
+    }
+    if(request.query.channel)
+    {
+	sql += ` ${kw} channel = ?`;
+	bindings.push(request.query.channel);
+	kw = "AND";
+    }
+    if(request.query.msgId)
+    {
+	sql += ` ${kw} msgId = ?`;
+	bindings.push(request.query.msgId);
+	kw = "AND";
+    }
+    
+    database.all(sql, bindings, (e, messageRows) => {
+	if(e) exit(`Error querying database: ${e}`);
+	else
+	{
+	    const sql = `SELECT * FROM Content`;
+	    database.all(sql, [], (e, contentRows) => {
+		if(e) exit(`Error querying database: ${e}`);
+		else
+		{
+		    var rows = messageRows;
+		    rows = rows.map(row => {
+			row.revisions = contentRows
+			    .filter(revision => revision.associatedMsg === row.msgId);
+			delete row.revisions.associatedMsg;
+			return row;
+		    });
+		    if(request.query.from)
+			rows = rows.filter(row => row.revisions[0].date >= strtotime(request.query.from));
+		    if(request.query.to)
+			rows = rows.filter(row => row.revisions[0].date < strtotime(request.query.to));
+		    if(request.query.as === "json") response.send(rows);
+		    else response.send(makeMessagePage(rows));
+		};
+	    });
+	};
+    });
 });
 
 // exit with error message
 function exit(message)
 {
-        console.error(message);
-        closeDatabase();
-        process.exit(1);
+    console.error(message);
+    closeDatabase();
+    process.exit(1);
 }
 
 // start the server!
 function main()
 {
-	// open the database
-	prepareDatabase();
+    // open the database
+    prepareDatabase();
 
-	// start the server listening
-	frontend.listen(config.port, () => console.log(`Server running on port ${config.port}!`));
+    // start the server listening
+    frontend.listen(config.port, () => console.log(`Server running on port ${config.port}!`));
 }
 
 // go!
